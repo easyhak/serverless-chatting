86ed559c9f1d42e4cd0f737d90d777ee10e15942
DESKTOP-1IBJ60D\JIHAK <zhzhk17@gmail.com>
2023-07-27 20:04:29 +0900

    Feat: channel 추가 함수
    - 단순히 db에 channel 추가만 되도록 함
    - 앞으로 구현해야 할 것 많음

---
+++ todos/add_channel.py
@@ -0,0 +1,39 @@
+import json
+import logging
+import os
+import time
+import uuid
+
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+
+
+def add_channel(event, context):
+    timestamp = str(time.time())
+
+    # table
+    user_table = dynamodb.Table("slack-user-pool")
+    channel_table = dynamodb.Table("channel_table")
+
+    # channel-table
+    # channel_name, users(null now.. 추후에 초대하면 추가), admin(username)
+
+    # slack-user-pool에 있는 것
+    #
+    # channel-table 현재 로그인 해 있는 user가 admin이 된다.
+    # pyjwt를 이용해 현재 사용자 알아내기
+
+    # channel_name 중복 처리 해줘야함
+
+    channel_item = {
+        'channel_name': event['channel_name'],  # partition key
+
+        'admin': "admin",
+        'createdAt': timestamp,
+        'updatedAt': timestamp,
+    }
+    channel_table.put_item(Item=channel_item)
+    print(event)
+
+    return channel_item
