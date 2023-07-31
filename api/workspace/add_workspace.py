f199372b293e4348e64a65ff9a15e52989b9d0d2
jihak <jooin2000@naver.com>
2023-07-31 13:44:53 +0900

    Refactor: initial

---
+++ api/workspace/add_workspace.py
@@ -0,0 +1,38 @@
+import json
+import logging
+import os
+import time
+import uuid
+
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+
+
+def add_channel(event, context):
+    timestamp = str(time.time())
+
+    # table
+    workspace_table = dynamodb.Table("workspace-table-dev")
+
+    # channel-table
+    # channel_name, users(null now.. 추후에 초대하면 추가), admin(username)
+
+    # slack-user-pool에 있는 것
+    #
+    # channel-table 현재 로그인 해 있는 user가 admin이 된다.
+    # pyjwt를 이용해 현재 사용자 알아내기
+
+    # channel_name 중복 처리 해줘야함
+
+    workspace_item = {
+        'workspace_name': event['workspace_name'],  # partition key
+        # workspace 안에 channels를 추가하기
+        'admin': "admin", # 만든 사람으로 하기
+        'createdAt': timestamp,
+        'updatedAt': timestamp,
+    }
+    workspace_table.put_item(Item=workspace_item)
+    print(event)
+
+    return workspace_item
