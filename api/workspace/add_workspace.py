46e082f2f023596628705f91197151e45057593b
DESKTOP-1IBJ60D\JIHAK <zhzhk17@gmail.com>
2023-08-01 21:42:25 +0900

    Feat: add workspace 기능 부분 완성

--- api/workspace/add_workspace.py
+++ api/workspace/add_workspace.py
@@ -1,5 +1,6 @@
 import json
 import time
+import uuid
 
 from api.dynamodb import get_dynamodb
 
@@ -13,21 +14,23 @@ def add_workspace(event, context):
     workspace_table = dynamodb.Table("workspace-table-dev")
 
     data = json.loads(event['body'])
-    # channel-table
-    # channel_name, users(null now.. 추후에 초대하면 추가), admin(username)
-
-    # slack-user-pool에 있는 것
-    #
+    # front에서 jwt -> 여기서 해독 -> email, username 등의 정보! -> email 사용
     # channel-table 현재 로그인 해 있는 user가 admin이 된다.
     # pyjwt를 이용해 현재 사용자 알아내기
 
-    # channel_name 중복 처리 해줘야함
+
+    # channel-table
+    # channel_name, users(null now.. 추후에 초대하면 추가), admin(username)
+
+
+    # workspace_name 중복 처리 해줘야 함
 
     workspace_item = {
-        'workspace_name': data['workspace_name'],  # partition key
+        'workspace_id': str(uuid.uuid1()),  # partition key
+        'workspace_name': data['workspace_name'],
         # workspace 안에 channels를 추가하기
 
-        'admin': "admin",  # 만든 사람으로 하기
+        'admin': "admin@admin.com",  # 만든 사람 email로 하기
         'createdAt': timestamp,
         'updatedAt': timestamp,
     }
