766b8698c5f37c34406c11f55d4cd7e1f329e5f7
DESKTOP-1IBJ60D\JIHAK <jooin2000@naver.com>
2023-08-05 22:29:54 +0900

    Feat: user가 속해있는 workspace의 정보를 알 수 있게해주는 api

--- api/workspace/get_workspaces.py
+++ api/workspace/get_workspaces.py
@@ -1,43 +1,44 @@
 import json
 
+import boto3
 from boto3.dynamodb.conditions import Attr
 
 from api import decimalencoder
 from api.dynamodb import get_dynamodb
 
-dynamodb = get_dynamodb()
+dynamodb = get_dynamodb()  # offline 가능
+user_dynamodb = boto3.resource('dynamodb')  # cloud table만 가져옴
 
+
+# /workspaces/{user}
+# user가 속해 있는 workspace list 반환
 def get_workspaces(event, context):
-    workspace_table = dynamodb.Table("main-table-dev")
+    user_table = user_dynamodb.Table("user-table")
+    main_table = dynamodb.Table("main-table-dev")
 
-    # fetch todo from the database
-    result = workspace_table.scan(
-        FilterExpression=Attr('member').contains('pathParameters')
+    user_email = "user#" + event['pathParameters']['user_email']
+    # user 정보를 가져옴
+    user_result = user_table.get_item(
+        Key={
+            'PK': user_email,
+            'SK': user_email
+        }
     )
-    # response = result['Item']
+    # 비어 있으면
+    print(user_result)
+    # valid user
+    try:
+        result = user_result['Item']
+        print(result['workspaces'])
+        # response = result['Item']
 
-    # create a response
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(result['Items'],
-                           cls=decimalencoder.DecimalEncoder)
-    }
-
-    return response
-# def get_workspaces(event, context):
-#     workspace_table = dynamodb.Table("workspace-table-dev")
-#
-#     # fetch todo from the database
-#     result = workspace_table.scan(
-#         FilterExpression=Attr('member').contains('pathParameters')
-#     )
-#     # response = result['Item']
-#
-#     # create a response
-#     response = {
-#         "statusCode": 200,
-#         "body": json.dumps(result['Items'],
-#                            cls=decimalencoder.DecimalEncoder)
-#     }
-#
-#     return response
+        return result['workspaces']
+    # invalid user
+    except:
+        response = {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "invalid user"
+            }, cls=decimalencoder.DecimalEncoder)
+        }
+        return response
