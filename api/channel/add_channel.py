75b6cbc3f4238217ceb2eaf30cfd81abfcea54d7
Nayeon Lee <ice1011@naver.com>
2023-08-07 00:03:15 +0900

    Feat: add_channel.py PK, SK 변경에 따른 코드 수정

--- api/channel/add_channel.py
+++ api/channel/add_channel.py
@@ -1,7 +1,9 @@
 import json
 import time
+import uuid
 
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 
 from api import decimalencoder
 from api.dynamodb import get_dynamodb
@@ -20,6 +22,8 @@ def add_channel(event, context):
     data = json.loads(event['body'])
     print(data)
 
+    channel_id = str(uuid.uuid4())
+
     # channel-table
     # channel_name, users(null now.. 추후에 초대하면 추가), admin(username)
 
@@ -29,21 +33,19 @@ def add_channel(event, context):
     # pyjwt를 이용해 현재 사용자 알아내기
 
     # workspace_name이 존재하는지 확인
-    workspace_item = channel_table.get_item(
-        Key={
-            'PK': 'workspace#' + data['workspace_name'],
-            'SK': 'workspace#' + data['workspace_name']
-        }
+    workspace_response = channel_table.scan(
+        FilterExpression=Attr('workspace_name').eq(data['workspace_name'])
     )
-    # workspace가 존재하지 않을 때 처리
-    try:
-        print(workspace_item['Item'])
-    except KeyError:
-        return {
+
+    # workspace가 존재하지 않는다면
+    if not workspace_response['Items']:
+        response = {
             "statusCode": 200,
-            "body": json.dumps({"message": data['workspace_name'] + " not exist"},
-                               cls=decimalencoder.DecimalEncoder)
+            "body": json.dumps({
+                "message": data['workspace_name'] + "not exist"
+            }, cls=decimalencoder.DecimalEncoder)
         }
+        return response
 
     """
     {
@@ -53,15 +55,11 @@ def add_channel(event, context):
     """
 
     # 이미 table에 channel name이 중복되는지 확인
-    channel_response = channel_table.get_item(
-        Key={
-            'PK': 'workspace#' + data['workspace_name'],
-            'SK': 'channel#' + data['channel_name']
-        }
+    channel_response = channel_table.scan(
+        FilterExpression=Attr('channel_name').eq(data['channel_name'])
     )
-    # 존재한다면, 예외 처리
-    try:
-        channel_response['Item']
+    # 중복 된다면 이거 {"message": "exist channel name"}
+    if channel_response['Items']:
         response = {
             "statusCode": 200,
             "body": json.dumps({
@@ -69,37 +67,41 @@ def add_channel(event, context):
             }, cls=decimalencoder.DecimalEncoder)
         }
         return response
-    except KeyError:
-        timestamp = str(time.time())
-        # table에 값 넣기
-        channel_item = {
-            'PK': 'workspace#' + data['workspace_name'],  # PK
-            'SK': 'channel#' + data['channel_name'],  # SK
-            'type': "channel",
-            # users, messages 부분..?
-            'users': [],
-            'messages': [],
-            'createdAt': timestamp,
-            'updatedAt': timestamp
-        }
 
-        channel_table.put_item(Item=channel_item)
 
-        # response 값 만들기
-        channel_info = {
-            "message": "channel created successfully",
-            "workspace": data['workspace_name'],
-            'type': "channel",
-            'users': [],
-            'messages': [],
-            'createdAt': timestamp,
-            'updatedAt': timestamp
-        }
+    timestamp = str(time.time())
+    # table에 값 넣기
+    channel_item = {
+        'PK': 'workspace#' + channel_id,  # PK
+        'SK': 'channel#' + channel_id,  # SK
+        'channel_name': data['channel_name'],
+        'workspace': data['workspace_id'],
+        'type': "channel",
+        # users, messages 부분..?
+        'users': [],
+        'messages': [],
+        'createdAt': timestamp,
+        'updatedAt': timestamp
+    }
 
-        response = {
-            "statusCode": 200,
-            "body": json.dumps(channel_info,
-                               cls=decimalencoder.DecimalEncoder)
-        }
+    channel_table.put_item(Item=channel_item)
 
-        return response
+    # response 값 만들기
+    channel_info = {
+        "message": "channel created successfully",
+        "workspace": data['workspace_name'],
+        'channel_id': channel_id,
+        'type': "channel",
+        'users': [data['user_email']],
+        'messages': [],
+        'createdAt': timestamp,
+        'updatedAt': timestamp
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(channel_info,
+                           cls=decimalencoder.DecimalEncoder)
+    }
+
+    return response
