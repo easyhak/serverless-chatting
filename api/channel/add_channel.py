3f6c4a1312279e406975a75648bffbadabf924f2
Nayeon Lee <ice1011@naver.com>
2023-08-06 17:33:32 +0900

    Feat: add_channel.py 기능 구현 완료 (user-table과 연결 미완)

--- api/channel/add_channel.py
+++ api/channel/add_channel.py
@@ -1,18 +1,24 @@
 import json
 import time
 
+import boto3
+
+from api import decimalencoder
 from api.dynamodb import get_dynamodb
 
 dynamodb = get_dynamodb()
-
+user_dynamodb = boto3.resource('dynamodb')  # cloud table만 가져옴
 
 def add_channel(event, context):
-    timestamp = str(time.time())
+
+    # todo
+    # user 부분 처리 안함
 
     # table
-    user_table = dynamodb.Table("slack-user-pool")
+    user_table = user_dynamodb.Table("user-table")
     channel_table = dynamodb.Table("main-table-dev")
     data = json.loads(event['body'])
+    print(data)
 
     # channel-table
     # channel_name, users(null now.. 추후에 초대하면 추가), admin(username)
@@ -21,23 +27,79 @@ def add_channel(event, context):
     #
     # channel-table 현재 로그인 해 있는 user가 admin이 된다.
     # pyjwt를 이용해 현재 사용자 알아내기
-    # workspace_name이 존재하는지 확인해줘야 함
-    # channel_name 중복 처리 해줘야함
+
+    # workspace_name이 존재하는지 확인
+    workspace_item = channel_table.get_item(
+        Key={
+            'PK': 'workspace#' + data['workspace_name'],
+            'SK': 'workspace#' + data['workspace_name']
+        }
+    )
+    # workspace가 존재하지 않을 때 처리
+    try:
+        print(workspace_item['Item'])
+    except KeyError:
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"message": data['workspace_name'] + " not exist"},
+                               cls=decimalencoder.DecimalEncoder)
+        }
+
     """
     {
         'workspace_name': workspace_name
         'channel_name': channel_name
     }
     """
-    channel_item = {
-        'PK': 'workspace#' + data['workspace_name'],  # PK
-        'SK': 'channel#' + data['channel_name'],  # SK
-        'type': 'channel',
-        'createdAt': timestamp,
-    }
 
+    # 이미 table에 channel name이 중복되는지 확인
+    channel_response = channel_table.get_item(
+        Key={
+            'PK': 'workspace#' + data['workspace_name'],
+            'SK': 'channel#' + data['channel_name']
+        }
+    )
+    # 존재한다면, 예외 처리
+    try:
+        channel_response['Item']
+        response = {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "exist channel name"
+            }, cls=decimalencoder.DecimalEncoder)
+        }
+        return response
+    except KeyError:
+        timestamp = str(time.time())
+        # table에 값 넣기
+        channel_item = {
+            'PK': 'workspace#' + data['workspace_name'],  # PK
+            'SK': 'channel#' + data['channel_name'],  # SK
+            'type': "channel",
+            # users, messages 부분..?
+            'users': [],
+            'messages': [],
+            'createdAt': timestamp,
+            'updatedAt': timestamp
+        }
 
-    channel_table.put_item(Item=channel_item)
-    print(channel_item)
+        channel_table.put_item(Item=channel_item)
 
-    return channel_item
+        # response 값 만들기
+        channel_info = {
+            "message": "channel created successfully",
+            "workspace": data['workspace_name'],
+            'type': "channel",
+            'users': [],
+            'messages': [],
+            'createdAt': timestamp,
+            'updatedAt': timestamp
+        }
+
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(channel_info,
+                               cls=decimalencoder.DecimalEncoder)
+        }
+
+        return response
