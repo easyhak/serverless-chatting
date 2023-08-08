09cd2118ca0ed727c3910ffb87224e63067fdbb8
Nayeon Lee <ice1011@naver.com>
2023-08-08 23:44:03 +0900

    Feat: get_channels.py user-table내 workspace, channel 구조에 따른 변경

--- api/channel/get_channels.py
+++ api/channel/get_channels.py
@@ -9,12 +9,10 @@ from api.dynamodb import get_dynamodb
 dynamodb = get_dynamodb()
 user_dynamodb = boto3.resource('dynamodb')  # cloud table만 가져옴
 
-# 하나의 워크스페이스 내 user가 속해 있는 channel list (channel name 담겨 있음) 반환
+# 하나의 workspace 내 user가 속해 있는 channel list (channel name? channel id?) 반환
 def get_channels(event, context):
     user_table = user_dynamodb.Table("user-table")
-    channel_table = dynamodb.Table("main-table-dev")
 
-    # 1. user가 valid한지 확인
     # user 정보를 가져옴
     user_email = "user#" + event['pathParameters']['user_email']
     user_result = user_table.get_item(
@@ -23,9 +21,13 @@ def get_channels(event, context):
             'SK': user_email
         }
     )
+
+    print(user_result)
+
     # valid user
     try:
         result = user_result['Item']
+        print(result['workspaces'])
     # invalid user
     except:
         response = {
@@ -36,51 +38,66 @@ def get_channels(event, context):
         }
         return response
 
-    # 2. workspace가 valid한지 확인 (workspace_name은 query parameter로?)
-    workspace_name = "workspace#" + event['queryStringParameters']['workspace_name']
-    workspace_item = channel_table.get_item(
-        Key={
-            'PK': workspace_name,
-            'SK': workspace_name
-        }
-    )
-    # workspace가 존재하지 않을 때 처리
-    try:
-        print(workspace_item['Item'])
-    except KeyError:
-        return {
-            "statusCode": 200,
-            "body": json.dumps({"message": event['pathParameters']['workspace_name'] + " not exist"},
-                               cls=decimalencoder.DecimalEncoder)
-        }
+    channels_list = []
+    workspace_id = event['pathParameters']['workspace_id']
 
-    # 3. workspace의 channel 내 유저가 존재한다면, 채널 리스트 목록에 추가
-
-    # main-table의 users에 담기는 값이 username이라고 가정
-    user_name = "user#" + event["pathParameters"]["username"]
-
-    # workspace 내 모든 채널 검색
-    channel_response = channel_table.query(
-        KeyConditionExpression="PK = :workspace_name AND begins_with(SK, :SK)",
+    # 해당 워크스페이스 정보를 가져옴
+    workspace_result = user_table.query(
+        KeyConditionExpression='workspaces.workspace_id = :workspace_id',
         ExpressionAttributeValues={
-            ":workspace_name": workspace_name,
-            ":SK": "channel#",
-        },
+            ':workspace_id': {'S': workspace_id}
+        }
     )
 
-    user_channels = []
+    for channels_result in workspace_result['Items']:
+        for channel in channels_result['channels']['L']:
+            channels_list.append(channel['S'])
 
-    for channel in channel_response["Items"]:
-        users_list = channel["users"]
-
-        if user_name in users_list:
-            user_channels.append(channel)
+    # # 2. workspace가 valid한지 확인 (workspace_name은 query parameter로?)
+    # workspace_name = "workspace#" + event['queryStringParameters']['workspace_name']
+    # workspace_item = channel_table.get_item(
+    #     Key={
+    #         'PK': workspace_name,
+    #         'SK': workspace_name
+    #     }
+    # )
+    # # workspace가 존재하지 않을 때 처리
+    # try:
+    #     print(workspace_item['Item'])
+    # except KeyError:
+    #     return {
+    #         "statusCode": 200,
+    #         "body": json.dumps({"message": event['pathParameters']['workspace_name'] + " not exist"},
+    #                            cls=decimalencoder.DecimalEncoder)
+    #     }
+    #
+    # # 3. workspace의 channel 내 유저가 존재한다면, 채널 리스트 목록에 추가
+    #
+    # # main-table의 users에 담기는 값이 username이라고 가정
+    # user_name = "user#" + event["pathParameters"]["username"]
+    #
+    # # workspace 내 모든 채널 검색
+    # channel_response = channel_table.query(
+    #     KeyConditionExpression="PK = :workspace_name AND begins_with(SK, :SK)",
+    #     ExpressionAttributeValues={
+    #         ":workspace_name": workspace_name,
+    #         ":SK": "channel#",
+    #     },
+    # )
+    #
+    # channels_list = []
+    #
+    # for channel in channel_response["Items"]:
+    #     users_list = channel["users"]
+    #
+    #     if user_name in users_list:
+    #         channels_list.append(channel)
 
 
     # create a response
     response = {
         "statusCode": 200,
-        "body": user_channels
+        "body": channels_list
     }
 
     return response
