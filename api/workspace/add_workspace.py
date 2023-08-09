44cdc151c98d18d1e2b09eee0e5fa7169b312d1f
DESKTOP-1IBJ60D\JIHAK <jooin2000@naver.com>
2023-08-09 19:55:25 +0900

    Fix: user-table에 workspace도 추가되도록 수정

--- api/workspace/add_workspace.py
+++ api/workspace/add_workspace.py
@@ -1,15 +1,22 @@
 import json
+import os
 import time
 import uuid
+
+import boto3
 
 from api import decimalencoder
 from api.dynamodb import get_dynamodb
 from boto3.dynamodb.conditions import Key, Attr
+from api.user.check_user import check_user
+
 dynamodb = get_dynamodb()
+user_dynamodb = boto3.resource('dynamodb')
 
 
 def add_workspace(event, context):
     table = dynamodb.Table("main-table-dev")
+    user_table = user_dynamodb.Table(os.environ['USER_TABLE'])
     data = json.loads(event['body'])
     print(data)
     workspace_id = str(uuid.uuid4())
@@ -18,7 +25,7 @@ def add_workspace(event, context):
     workspace_response = table.scan(
         FilterExpression=Attr('workspace_name').eq(data['workspace_name'])
     )
-    print( workspace_response['Items'])
+    print(workspace_response['Items'])
     # 존재 한다면 예외 처리
 
     # 중복 된다면 이거 {"message": "exist workspace name"}
@@ -33,9 +40,32 @@ def add_workspace(event, context):
 
     timestamp = str(time.time())
     # table 값 넣기
+
+    if check_user("user#" + data['user_email']):
+
+        user_table.update_item(
+            Key={
+                'PK': "user#" + data['user_email'],
+                'SK': "user#" + data['user_email']
+            },
+            UpdateExpression="SET workspaces = list_append(workspaces, :i)",
+            ExpressionAttributeValues={
+                ':i': [workspace_id],
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+    else:
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "not valid user"
+            },
+                cls=decimalencoder.DecimalEncoder)
+        }
+
     workspace_item = {
-        'PK': "workspace#"+workspace_id,
-        'SK': "workspace#"+workspace_id,
+        'PK': "workspace#" + workspace_id,
+        'SK': "workspace#" + workspace_id,
         # 'PK': 'workspace#' + data['workspace_name'],  # partition key
         # 'SK': 'workspace#' + data['workspace_name'],  # sort key
         # workspace 안에 channels를 추가하기
@@ -49,7 +79,6 @@ def add_workspace(event, context):
     }
 
     table.put_item(Item=workspace_item)
-
     # response 값 만들기
     workspace_info = {
         "message": "workspace created successfully",
