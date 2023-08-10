d05766d2b6f03eddbe549d527b6041a77fdfe69d
DESKTOP-1IBJ60D\JIHAK <jooin2000@naver.com>
2023-08-11 01:28:45 +0900

    Fix: user-table에서 channel 정보도 넣을 수 있도록 수정

--- api/channel/delete_channel.py
+++ api/channel/delete_channel.py
@@ -1,21 +1,24 @@
 import json
 import os
 
+import boto3
+
 from api import decimalencoder
 from api.dynamodb import get_dynamodb
 
 dynamodb = get_dynamodb()
+user_dynamodb = boto3.resource('dynamodb')  # cloud table만 가져옴
 
 
 def delete_channel(event, context):
     # table
     channel_table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    user_table = user_dynamodb.Table(os.environ['USER_TABLE'])
 
     workspace_id = "workspace#" + event['pathParameters']['workspace_id']
     channel_id = "channel#" + event['pathParameters']['channel_id']
     print(channel_id)
 
-    # delete the todo from the database
     channel_item = channel_table.get_item(
         Key={
             'PK': workspace_id,
@@ -29,10 +32,32 @@ def delete_channel(event, context):
     except KeyError:
         return {
             "statusCode": 200,
-            # 이 부분 접근하는 방법이 이게 맞나?
+
             "body": json.dumps({"message": event['pathParameters']['channel_id'] + " not exist"},
                                cls=decimalencoder.DecimalEncoder)
         }
+    response = user_table.scan()
+    for x in response['Items']:
+        for i, y in enumerate(x['workspaces']):
+            print(list(y.keys())[0])
+            # 같으면 지우기
+            if event['pathParameters']['workspace_id'] == list(y.keys())[0]:
+                for j, z in enumerate(list(y.values())[0]):
+                    print(z)
+                    if z == event['pathParameters']['channel_id']:
+                        query = "REMOVE workspaces[%d].%s[%d]" % (i, event['pathParameters']['workspace_id'], j)
+                        user_table.update_item(
+                            Key={
+                                "PK": x['PK'],
+                                "SK": x['SK']
+                            },
+                            UpdateExpression=f"REMOVE #src[{i}].#workspace_id[{j}]",
+                            ExpressionAttributeNames={
+                                '#src': 'workspaces',
+                                # '#ind': str(ind),
+                                '#workspace_id': event['pathParameters']['workspace_id']
+                            },
+                        )
 
     channel_table.delete_item(
         Key={
