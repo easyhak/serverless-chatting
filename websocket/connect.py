52725d4722d19f5b7fc7dc3bd89297772307cb48
jihak <jooin2000@naver.com>
2023-08-19 17:45:22 +0900

    Feat: connect disconnect 할 때 connection 여부 파악

--- websocket/connect.py
+++ websocket/connect.py
@@ -1,34 +1,50 @@
+import os
+import time
+
 import boto3
 import string
 from api.jwt.jwt_decoder import decode
+from api.user.check_user import check_user
+
 dynamodb = boto3.client('dynamodb')
+
+user_dynamodb = boto3.resource('dynamodb')
 
 
 def connect(event, context):
     connectionId = event['requestContext']['connectionId']
+    timestamp = int(time.time() * 1000)
+    user_table = user_dynamodb.Table(os.environ['USER_TABLE'])
     print(event)
-    print("-------------------headers---------------")
-    print(event['headers'])
-    print("-------------------requestContext---------------")
-    print(event['requestContext'])
     # id token을 복호화 email, nickname
     # table 에다가 저장
     try:
-        print(decode(event['headers']['IDToken'])['email']) # 연결된 id를 반환하도록 함
+        # user가 존재하지 않으면 disconnect/401 unauthorized error
+        # user가 존재하면 connecting = true
+
+        user_email = decode(event['headers']['IDToken'])['email']
+        print(user_email)  # 연결된 id를 반환하도록 함
+        # user가 있는지 확인
+        item = {
+            'PK': "user#" + user_email,
+            'SK': "connectionId#" + connectionId,
+            'createdAt': timestamp,
+            'updatedAt': timestamp,
+        }
+        if check_user("user#" + user_email):
+            user_table.put_item(Item=item)
+        else:
+            return {
+                'statusCode': 401,
+                'body': 'Authorization error'
+            }
+
     except KeyError:
         print("no id token")
-    '''
-    {
-    'routeKey': '$connect', 'eventType': 'CONNECT', 
-    'extendedRequestId': 'IQQoWHgeoE0FZ7Q=', 'requestTime': '18/Jul/2023:09:54:48 +0000', 
-    'messageDirection': 'IN', 'stage': 'dev', 'connectedAt': 1689674088348,
-    'requestTimeEpoch': 1689674088349, 'identity': {'sourceIp': '128.134.206.230'},
-    'requestId': 'IQQoWHgeoE0FZ7Q=', 'domainName': '12vtsljh67.execute-api.ap-northeast-2.amazonaws.com', 
-    'connectionId': 'IQQoWeWEoE0CFfQ=', 'apiId': '12vtsljh67'
-    }
-    '''
-    # random nickname generator
-    _LENGTH = 10
-    string_pool = string.ascii_lowercase  # 소문자
+        return {
+            'statusCode': 401,
+            'body': 'Authorization error'
+
+        }
 
     return {}
--- websocket/disconnect.py
+++ websocket/disconnect.py
@@ -1,16 +1,33 @@
 import json
+import time
+
 import boto3
 import os
 
+from api.jwt.jwt_decoder import decode
+from api.user.check_user import check_user
+
+from boto3.dynamodb.conditions import Attr
+
 dynamodb = boto3.client('dynamodb')
+user_dynamodb = boto3.resource('dynamodb')
 
 
 def disconnect(event, context):
+    timestamp = int(time.time() * 1000)
+    user_table = user_dynamodb.Table(os.environ['USER_TABLE'])
     connectionId = event['requestContext']['connectionId']
-
-    dynamodb.delete_item(
-        TableName=os.environ['WEBSOCKET_TABLE'],
-        Key={'connectionId': {'S': connectionId}}
+    response = user_table.scan(
+        FilterExpression=Attr('SK').eq("connectionId#" + connectionId)
+    )
+    print(response['Items'])
+    user_email = response['Items'][0]['PK']
+    print(user_email)
+    user_table.delete_item(
+        Key={
+            'PK': user_email,
+            'SK': "connectionId#" + connectionId
+        }
     )
 
     return {}
