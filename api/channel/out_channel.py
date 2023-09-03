6cb5a89edd798678e749dec4e3fe3b7e1b1d8439
Nayeon Lee <ice1011@naver.com>
2023-09-03 15:00:06 +0900

    Fix: channel 나가기 test 완료

--- api/channel/out_channel.py
+++ api/channel/out_channel.py
@@ -8,6 +8,7 @@ from api.dynamodb import get_dynamodb
 dynamodb = get_dynamodb()
 user_dynamodb = boto3.resource('dynamodb')  # cloud table만 가져옴
 
+
 #  채널의 users list에서 user 삭제
 # user의 channel 삭제
 
@@ -21,23 +22,32 @@ def out_channel(event, context):
     channel_id = "channel#" + data['channel_id']
     user_email = "user#" + data['user_email']
 
+    res = table.get_item(
+        Key={
+            'PK': workspace_id,
+            'SK': channel_id
+        }
+    )
+    print(res['Item']['users'])
+    # 몇번 째 index인지 알아내기
 
+    user_index = res['Item']['users'].index(data['user_email'])
+    print(user_index)
     try:
+
         table_response = table.update_item(
             Key={
                 'PK': workspace_id,
                 'SK': channel_id
             },
-            UpdateExpression="REMOVE #users :i",
+            UpdateExpression=f"REMOVE #src[{user_index}]",
 
-            ExpressionAttributeValues={
-                ':i': [data['user_email']]
-            },
             ExpressionAttributeNames={
-                '#users': 'users',
-
+                '#src': 'users'
             }
+
         )
+        print(table_response)
     except:
         response = {
             "statusCode": 200,
@@ -47,7 +57,7 @@ def out_channel(event, context):
         }
         return response
 
-
+    # user table에서 channel_id 삭제
     user_item = user_table.get_item(
         Key={
             'PK': user_email,
@@ -55,14 +65,28 @@ def out_channel(event, context):
         }
     )
 
-    ind = -1
+    work_idx = -1
     for i, x in enumerate(user_item['Item']['workspaces']):
         print("===========")
         print(list(x.keys())[0])
         if list(x.keys())[0] == data['workspace_id']:
-            ind = i
+            work_idx = i
+            for j, y in enumerate(list(x.values())[0]):
+                print(y)
+                if y == data['channel_id']:
+                    user_table.update_item(
+                        Key={
+                            "PK": user_email,
+                            "SK": user_email
+                        },
+                        UpdateExpression=f"REMOVE #src[{work_idx}].#workspace_id[{j}]",
+                        ExpressionAttributeNames={
+                            '#src': 'workspaces',
+                            '#workspace_id': data['workspace_id']
+                        },
+                    )
             break
-    if ind == -1:
+    if work_idx == -1:
         response = {
             "statusCode": 200,
             "body": json.dumps({
@@ -70,29 +94,11 @@ def out_channel(event, context):
             }, cls=decimalencoder.DecimalEncoder)
         }
         return response
-    print(ind)
-
-    # user table에서 channel_id 삭제
-    user_table.update_item(
-        Key={
-            'PK': user_email,
-            'SK': user_email
-        },
-        UpdateExpression=f"REMOVE #src[{ind}].#workspace_id :i",
-
-        ExpressionAttributeValues={
-            ':i': [data['channel_id']]
-        },
-        ExpressionAttributeNames={
-            '#src': 'workspaces',
-            '#workspace_id': data['workspace_id']
-        }
-    )
 
     # create a response
     response = {
         "statusCode": 200,
-        "body": json.dumps({"message":  "channel out complete"},
+        "body": json.dumps({"message": "channel out complete"},
                            cls=decimalencoder.DecimalEncoder)
     }
 
